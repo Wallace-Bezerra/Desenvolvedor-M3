a00b0178352d46fb3597ef7588ee4b961fede716
Wallace-Bezerra <89711908+Wallace-Bezerra@users.noreply.github.com>
2022-11-27 21:26:26 -0300

    projeto-feito-ajustes-finais

--- src/js/index.js
+++ src/js/index.js
@@ -1,23 +1,20 @@
-const serverurl = process.env.SERVER_API;
 let dataProducts = [];
 let filtroGlobal = [];
+let filterPriceGlobal = [];
 let order = highPriceds;
-let limit = 6;
-console.log("Dev m3", serverurl);
+let limiteAtual = 6;
+let cartItens = 0;
+
+fetchProducts();
 
 //carrinho implementação
-const root = document.querySelector(":root");
-const teste = root.style.getPropertyValue("--text");
-// console.log(teste)
-root.addEventListener("click", () => {
-  root.style.setProperty("--text", "'1'");
-  // console.log(root.style.getPropertyValue("--text"));
-});
+const cartItem = document.querySelector(".cartItem");
 
 // filtro seleção
 const iconCart = document.querySelector(".icon-cart");
 const filterSelect = document.querySelector(".filter-select");
 const filterOption = document.querySelector(".filter-options");
+const filterMobile = document.querySelector(".filterMobile.order");
 
 filterSelect.addEventListener("click", () => {
   filterOption.classList.toggle("active");
@@ -28,23 +25,19 @@ filterSelect.addEventListener("click", () => {
 const moreColors = document.querySelector(".more-colors");
 const moreColorsData = document.querySelector("[data-moreColors]");
 moreColors.addEventListener("click", () => {
-  // moreColors.dataset.dataMoreColors = "active";
   if (moreColorsData.dataset.morecolors === "active") {
     moreColorsData.dataset.morecolors = "";
   } else {
     moreColorsData.dataset.morecolors = "active";
   }
-  // console.log(moreColorsData.dataset.morecolors);
 });
 
 // gridSize
 
 const itemSize = document.querySelectorAll(".item-size");
-// console.log(itemSize);
 itemSize.forEach((item) => {
   item.addEventListener("click", () => {
     item.classList.toggle("active");
-    // console.log(item);
   });
 });
 
@@ -60,26 +53,34 @@ const options = {
 const formatNumber = new Intl.NumberFormat("pt-BR", options);
 
 async function fetchProducts() {
-  const data = await fetch("http://localhost:5000/products");
-  const response = await data.json();
-  //  getProducts(response);
-  dataProducts = response.map((item) => {
-    return item;
-  });
-  renderProducts(response);
-  filtroGlobal = dataProducts;
-}
+  try {
+    const data = await fetch("http://localhost:5000/products");
+    const response = await data.json();
+    dataProducts = response.map((item) => {
+      return item;
+    });
 
-// function getProducts(products){
-//   const productsArray = products.map((item)=>{
-//     return item
-//    })
-//    console.log(productsArray)
-// }
+    renderProducts(response);
+    filtroGlobal = dataProducts;
+  } catch (error) {
+    productsGrid.innerHTML = `
+    <div style="color:red;">
+      <p style="margin-bottom:10px;">Não foi possivel realizar o carregamento</p> 
+      <p>ERRO:${error}</p>
+    </div>`;
+  }
+
+  const btnCards = document.querySelectorAll("button");
+  btnCards.forEach((item) => {
+    item.addEventListener("click", () => {
+      cartItem.innerText = cartItens += 1;
+    });
+  });
+}
 
 function renderProducts(products) {
   products.forEach((item, index) => {
-    if (index < limit) {
+    if (index < limiteAtual) {
       productsGrid.innerHTML += `
           <li class="card-item">
             <img src=${item.image} alt="">
@@ -95,69 +96,85 @@ function renderProducts(products) {
       `;
     }
   });
-  limit = 6;
+  if (loadingMore.classList.contains("disabled") && filtroGlobal.length > 6) {
+    loadingMore.classList.remove("disabled");
+    limiteAtual = 6;
+  }
+  // limiteAtual = 6;
 }
 
-fetchProducts();
-
 // ordenação
-const highPriced = document.querySelector(".high-priced");
-const smallPrice = document.querySelector(".small-price");
-const recentSelect = document.querySelector(".recent");
-// console.log(highPriced);
 
-highPriced.addEventListener("click", highPriceds);
-smallPrice.addEventListener("click", smallPrices);
-recentSelect.addEventListener("click", recent);
+const highPriced = document.querySelectorAll(".high-priced");
+const smallPrice = document.querySelectorAll(".small-price");
+const recentSelect = document.querySelectorAll(".recent");
+
+highPriced.forEach((item) => {
+  item.addEventListener("click", highPriceds);
+});
+
+smallPrice.forEach((item) => {
+  item.addEventListener("click", smallPrices);
+});
+
+recentSelect.forEach((item) => {
+  item.addEventListener("click", recent);
+});
 
 function highPriceds() {
-  filtroGlobal.sort(function (a, b) {
+  filtroGlobal.sort((a, b) => {
     return a.price < b.price ? 1 : a.price > b.price ? -1 : 0;
   });
+  setTimeout(() => {
+    filterOption.classList.remove("active");
+  }, 800);
+  if (filterMobile.classList.contains("active")) {
+    filterMobile.classList.remove("active");
+  }
   clearProducts();
   renderProducts(filtroGlobal);
   order = highPriceds;
-  // console.log(filtroGlobal);
-  // console.log(order)
 }
 
 function smallPrices() {
-  filtroGlobal.sort(function (a, b) {
+  filtroGlobal.sort((a, b) => {
     return a.price < b.price ? -1 : a.price > b.price ? 1 : 0;
   });
+  setTimeout(() => {
+    filterOption.classList.remove("active");
+  }, 800);
+  if (filterMobile.classList.contains("active")) {
+    filterMobile.classList.remove("active");
+  }
   clearProducts();
   renderProducts(filtroGlobal);
   order = smallPrices;
-  // console.log(filtroGlobal);
-  // console.log(order)
 }
 
 function recent() {
-  filtroGlobal.sort(function (a, b) {
+  filtroGlobal.sort((a, b) => {
     const date1 = new Date(a.date);
     const date2 = new Date(b.date);
     return date2 - date1;
   });
+  setTimeout(() => {
+    filterOption.classList.remove("active");
+  }, 800);
+
+  if (filterMobile.classList.contains("active")) {
+    filterMobile.classList.remove("active");
+  }
   clearProducts();
   renderProducts(filtroGlobal);
   order = recent;
-  // console.log(filtroGlobal);
-  // console.log(order)
 }
 
 function clearProducts() {
-  const cardsItems = document.querySelectorAll(".card-item");
-  cardsItems.forEach((item) => {
-    item.remove();
-  });
-  // productsGrid.innerHTML = "";
+  productsGrid.innerHTML = "";
 }
 
 // filtro cor
 
-// preciso deixar esse evento generico e filtrar com
-
-//tentar utilizar dataset
 const checkboxColors = document.querySelectorAll(
   "[data-colors='colors'] input[type='checkbox']"
 );
@@ -171,19 +188,15 @@ const colors = [];
 const sizes = [];
 const prices = [];
 
-console.log(checkboxColors);
-console.log(checkboxSizes);
-console.log(checkboxPrices);
-
 checkboxColors.forEach((item) => {
   item.addEventListener("change", () => {
-    if (item.checked && !colors.includes(item.getAttribute("id"))) {
-      colors.push(item.getAttribute("id"));
+    if (item.checked && !colors.includes(item.getAttribute("class"))) {
+      colors.push(item.getAttribute("class"));
       filtroGlobal = [];
       filterColor(colors, filtroGlobal);
     } else {
       if (!item.checked) {
-        colors.splice(colors.indexOf(item.id), 1);
+        colors.splice(colors.indexOf(item.getAttribute("class")), 1);
         if (colors.length > 0) {
           filtroGlobal = [];
           filterColor(colors, filtroGlobal);
@@ -194,13 +207,6 @@ checkboxColors.forEach((item) => {
         }
       }
     }
-
-    if (
-      loadingMore.classList.contains("disabled") &&
-      filtroGlobal.length > limit
-    ) {
-      loadingMore.classList.remove("disabled");
-    }
   });
 });
 
@@ -208,12 +214,10 @@ checkboxSizes.forEach((item) => {
   item.addEventListener("change", () => {
     if (item.checked && !sizes.includes(item.getAttribute("id"))) {
       sizes.push(item.getAttribute("id"));
-      // filtroGlobal = [];
-      console.log(sizes);
       filterSize(sizes, filtroGlobal);
     } else {
       if (!item.checked) {
-        sizes.splice(sizes.indexOf(item.id), 1);
+        sizes.splice(sizes.indexOf(item.getAttribute("id")), 1);
         if (sizes.length > 0) {
           filtroGlobal = [];
           filterSize(sizes, filtroGlobal);
@@ -226,134 +230,69 @@ checkboxSizes.forEach((item) => {
     }
   });
 });
+
 checkboxPrices.forEach((item) => {
   item.addEventListener("change", () => {
-    if (item.checked && !prices.includes(item.getAttribute("id"))) {
-      prices.push(item.getAttribute("id"));
-      // filtroGlobal = [];
-      console.log(prices);
-      // filterSize(price, filtroGlobal);
+    if (item.checked && !prices.includes(item.getAttribute("class"))) {
+      prices.push(item.getAttribute("class"));
+      filterPrice(prices, filtroGlobal);
+    } else {
+      if (!item.checked) {
+        prices.splice(prices.indexOf(item.getAttribute("class")), 1);
+        if (prices.length > 0) {
+          filtroGlobal = [];
+          filterSize(prices, filtroGlobal);
+        } else {
+          clearProducts();
+          filtroGlobal = dataProducts;
+          renderProducts(dataProducts);
+        }
+      }
     }
   });
 });
 
-// checkbox.forEach((item) => {
-//   const parentElementes = item.parentElement.parentElement.dataset;
-//   item.addEventListener("change", (event) => {
-//     console.log(filtroGlobal, limit, "consoles")
-
-//     const idItem = event.currentTarget.getAttribute("id");
-//     console.log(idItem);
-
-//     // const data = Object.values(JSON.parse(JSON.stringify(teste1)));
-//     // console.log(data);
-//     // console.log(data[0]);
-
-//     if (item.checked) {
-//       checkbox.forEach((item) => {
-//         const ife = item.checked && parentElementes.colors === "colors" && !colors.includes(item.getAttribute("id"));
-//         console.log(ife,item,parentElementes.colors)
-//         if (ife) {
-//           console.log(item.getAttribute("id"),"chove");
-//           // console.log(data[0],"teste 0000")
-//           console.log("colors antes",colors)
-//           colors.push(item.getAttribute("id"));
-//           // console.log(item, "checked")
-//           // console.log(colors);
-//           console.log(colors,"colors depois");
-//           filtroGlobal = []
-//           filterColor(colors, filtroGlobal)
-//           // console.log(filtroGlobal, "globals")
-//         }
-//         // if((item.checked && parentElementes.sizes === "sizes" && !sizes.includes(item.getAttribute("id")))){
-//         //   console.log(sizes,"sizes antes")
-//         //   // sizes.push(item.getAttribute("id"));
-//         //   // filtroGlobal = []
-//         //   console.log(sizes,"sizes depois");
-//         //   // filterSize(sizes, filtroGlobal);
-//         // }
-
-//       })
-//     }
-//     else {
-//       // clearProducts();
-
-//       // const teste = colors.slice(colors.indexOf(item.id), 2);
-//       // colors.push("fudeu")
-//       // console.log("deu certo")
-//       // console.log(colors.indexOf(item.id));
-//       // console.log(item.id)
-//       if (!item.checked ) {
-
-//         if (colors.length > 0 && parentElementes.colors === "colors") {
-//           colors.splice(colors.indexOf(item.id), 1);
-//           // console.log(colors, "cores")
-//           filtroGlobal = [];
-//           // console.log("filtrado..")
-//           filterColor(colors, filtroGlobal)
-//           console.log("filtrou")
-//           // console.log(filtroGlobal, "teste Global");
-//         }
-
-//         // if(sizes.length > 0 && data[0] === "sizes"){
-//         //   sizes.splice(sizes.indexOf(item.id), 1);
-//         //   filtroGlobal = [];
-//         //   console.log("filtrou size")
-//         //   console.log(sizes)
-//         //   // filterSize(colors, filtroGlobal)
-//         //   clearProducts();
-//         //   filtroGlobal = dataProducts;
-//         //   renderProducts(dataProducts);
-//         // }
-//         else {
-//           clearProducts();
-//           filtroGlobal = dataProducts;
-//           renderProducts(dataProducts);
-//           console.log("limpou")
-//         }
-
-//       }
-//     }
-//     if (loadingMore.classList.contains("disabled") && filtroGlobal.length > limit) {
-//       loadingMore.classList.remove("disabled")
-//       // loadingMore.setAttribute("disabled", "");
-//       // console.log("aloo")
-//     }
-//   })
-// })
-
-function filterColor(color, filtro) {
+function filterColor(color) {
   color.forEach((colorItem) => {
-    console.log(filtroGlobal, "global");
     const filter = dataProducts.filter((item) => {
       return item.color.toLocaleUpperCase() === colorItem.toLocaleUpperCase();
     });
-    // console.log(filtroGlobal, "filtro antes ")
-    // console.log(filtro, "filtro!")
     filtroGlobal = [...filtroGlobal, ...filter];
-    // console.log(dataProducts, "dataproducts")
-    console.log(filtroGlobal, "Estou Testando filtro depois", colorItem);
     clearProducts();
     renderProducts(filtroGlobal);
     order();
   });
 }
 
-function filterSize(size, filtro) {
+function filterSize(size) {
   size.forEach((sizeItem) => {
-    console.log(filtroGlobal);
-    console.log(sizeItem, "sizeee");
     const filter = filtroGlobal.filter((item) => {
       return item.size[0].toLocaleUpperCase() === sizeItem.toLocaleUpperCase();
     });
-    // console.log(filtroGlobal, "filtro antes ")
-    // console.log(filtro, "filtro!")
     filtroGlobal = [...filter];
-    // console.log(dataProducts, "dataproducts")
-    console.log(filtroGlobal, "Estou Testando filtro depois", sizeItem);
     clearProducts();
     renderProducts(filtroGlobal);
-    // order();
+  });
+}
+function filterPrice(prices) {
+  prices.forEach((priceItem) => {
+    const pricesObj = {
+      min: priceItem.split("-")[0],
+      max: priceItem.split("-")[1],
+    };
+    const filter = dataProducts.filter((item) => {
+      return item.price >= pricesObj.min && item.price <= pricesObj.max;
+    });
+    if (prices.length > 1) {
+      filtroGlobal = [...filterPriceGlobal, ...filter];
+      clearProducts();
+      renderProducts(filtroGlobal);
+    } else {
+      filterPriceGlobal = [...filterPriceGlobal, ...filter];
+      filtroGlobal = [...filter];
+      clearProducts();
+      renderProducts(filtroGlobal);
+    }
   });
 }
 
@@ -361,16 +300,42 @@ function filterSize(size, filtro) {
 
 const loadingMore = document.querySelector(".loading-more");
 loadingMore.addEventListener("click", () => {
-  // limit += 3;
-  if (filtroGlobal.length > limit) {
-    limit = filtroGlobal.length;
-    // loadingMore.remove();
-    // loadingMore.setAttribute("disabled", true);
-    loadingMore.classList.add("disabled");
+  if (filtroGlobal.length > limiteAtual) {
+    limiteAtual = filtroGlobal.length;
     clearProducts();
     renderProducts(filtroGlobal);
-  } else {
-    alert("nao tem mais");
-    // console.log(filtroGlobal)
+    loadingMore.classList.add("disabled");
   }
+});
+
+// click no filtro mobile
+const btnMobile = document.querySelectorAll(".btn.mobile");
+const btnCloseMobile = document.querySelectorAll(".btnClose");
+
+btnMobile.forEach((item) => {
+  item.addEventListener("click", () => {
+    if (item.innerText.toLocaleUpperCase() === "FILTRAR") {
+      const filterMobile = document.querySelector(".filterMobile.filter");
+      filterMobile.classList.add("active");
+    }
+    if (item.innerText.toLocaleUpperCase() === "ORDENAR") {
+      const filterMobile = document.querySelector(".filterMobile.order");
+      filterMobile.classList.add("active");
+    }
+  });
+  btnCloseMobile.forEach((item) => {
+    item.addEventListener("click", () => {
+      item.parentElement.parentElement.classList.remove("active");
+    });
+  });
+});
+
+// accordion mobile
+
+const btnAccordion = document.querySelectorAll(".arrow-accordion");
+btnAccordion.forEach((item) => {
+  item.addEventListener("click", (event) => {
+    item.parentElement.nextElementSibling.classList.toggle("active");
+    event.currentTarget.classList.toggle("active");
+  });
 });
